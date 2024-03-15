96d8b4e1e3f920538a03f3462b9b95522f7a94a9
DendyLine <anatolijrivne@gmail.com>
2024-03-15 23:54:23 +0200

    all icons/pictures added, variables created, started to make header

---
+++ ignat-project/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './assets/components'
+import {Header} from "./assets/components";
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <Header/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ ignat-project/src/assets/components/Header.tsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+    return(
+        <div className = 'Header'>
+            <div className = 'wFrame'></div>
+            <div className = 'middle'>
+                <div className = 'left'>
+                    <div><a>098 900 09 09</a></div>
+                    <div><a>Допомога</a></div>
+                </div>
+                <div className = 'right'><a>Увійти</a>/<a>Зареєструватися</a></div>
+            </div>
+            <div className = 'bottom'></div>
+        </div>
+    )
+}
